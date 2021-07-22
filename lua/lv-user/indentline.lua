d478e19c813ea2f8e3934612ddde0eb6a806c523
nawfalmrouyan <nawfal@gmail.com>
2021-07-22 22:45:37 +0800

    prettier indentlines

--- lua/lv-user/indentline.lua
+++ lua/lv-user/indentline.lua
@@ -1,8 +1,5 @@
 local M = {}
 M.config = function()
-  vim.g.indentLine_enabled = 1
-  vim.g.indent_blankline_char = "│"
-
   vim.g.indent_blankline_filetype_exclude = {
     "help",
     "terminal",
@@ -14,37 +11,42 @@ M.config = function()
     "LICENSE",
   }
   vim.g.indent_blankline_buftype_exclude = { "qf", "terminal" }
-  vim.g.indent_blankline_show_current_context = true
-  vim.g.indentLine_faster = 1
 
-  vim.g.indent_blankline_show_trailing_blankline_indent = false
-  vim.g.indent_blankline_show_first_indent_level = true
-
-  -- vim.g.indent_blankline_space_char_blankline = '•'
-  -- vim.g.indent_blankline_space_char = '•'
-  -- vim.g.indent_blankline_use_treesitter = true
-  vim.g.indent_blankline_context_patterns = {
-    "^for",
-    "^if",
-    "^object",
-    "^table",
-    "^while",
-    "arguments",
-    "block",
-    "catch_clause",
-    "class",
-    "else_clause",
-    "function",
-    "if_statement",
-    "import_statement",
-    "jsx_element",
-    "jsx_self_closing_element",
-    "method",
-    "operation_type",
-    "return",
-    "try_statement",
+  vim.cmd [[highlight IndentOne guifg=#BF616A guibg=NONE gui=nocombine]]
+  vim.cmd [[highlight IndentTwo guifg=#D08770 guibg=NONE gui=nocombine]]
+  vim.cmd [[highlight IndentThree guifg=#EBCB8B guibg=NONE gui=nocombine]]
+  vim.cmd [[highlight IndentFour guifg=#A3BE8C guibg=NONE gui=nocombine]]
+  vim.cmd [[highlight IndentFive guifg=#5E81AC guibg=NONE gui=nocombine]]
+  vim.cmd [[highlight IndentSix guifg=#88C0D0 guibg=NONE gui=nocombine]]
+  vim.cmd [[highlight IndentSeven guifg=#B48EAD guibg=NONE gui=nocombine]]
+  vim.g.indent_blankline_char = "│"
+  vim.g.indent_blankline_char_highlight_list = {
+    "IndentOne",
+    "IndentTwo",
+    "IndentThree",
+    "IndentFour",
+    "IndentFive",
+    "IndentSix",
+    "IndentSeven",
   }
-  -- vim.g.indent_blankline_char_highlight_list = {'Whitespace', 'LineNr'}
+  vim.g.indent_blankline_show_first_indent_level = true
+  vim.g.indent_blankline_show_trailing_blankline_indent = false
+  vim.g.indent_blankline_show_current_context = true
+  vim.g.indent_blankline_context_patterns = {
+    "class",
+    "function",
+    "method",
+    "block",
+    "list_literal",
+    "selector",
+    "^if",
+    "^table",
+    "if_statement",
+    "while",
+    "for",
+  }
+  -- because lazy load indent-blankline so need readd this autocmd
+  vim.cmd "autocmd CursorMoved * IndentBlanklineRefresh"
 
   O.user_which_key["i"] = { "<cmd>IndentBlanklineToggle<CR>", "Indent Lines" }
 end
