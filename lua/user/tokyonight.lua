3401aee13523083e487c81d9ddc4217de2431aec
Nawfal <nawfalmrouyan@gmail.com>
2021-08-17 22:06:40 +0800

    disable transparent for tokyonight theme

--- lua/user/tokyonight.lua
+++ lua/user/tokyonight.lua
@@ -5,12 +5,12 @@ vim.g.tokyonight_italic_comments = true
 vim.g.tokyonight_italic_keywords = true
 vim.g.tokyonight_italic_functions = true
 vim.g.tokyonight_italic_variables = true
-vim.g.tokyonight_transparent = true
-vim.g.tokyonight_transparent_sidebar = true
+vim.g.tokyonight_transparent = false
+vim.g.tokyonight_transparent_sidebar = false
 -- vim.g.tokyonight_dark_sidebars = true
 -- vim.g.tokyonight_sidebars = { "qf", "NvimTree", "floaterm", "Symbols", "terminal", "packer" }
 -- vim.g.tokyonight_dark_float = true
-vim.g.tokyonight_lualine_bold = true
+-- vim.g.tokyonight_lualine_bold = true
 vim.g.tokyonight_hide_inactive_statusline = true
 
 -- Change the "hint" color to the "orange" color, and make the "error" color bright red
