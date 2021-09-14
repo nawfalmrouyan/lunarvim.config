cd1db7c4cf2e68f947f47d89eb3c154301841c48
Nawfal <nawfalmrouyan@gmail.com>
2021-09-14 22:21:32 +0800

    disable neorg and enable orgmode plugin

--- config.lua
+++ config.lua
@@ -342,33 +342,49 @@ lvim.plugins = {
     event = "BufEnter",
   },
   {
-    "wellle/targets.vim",
-  },
-  {
-    "nvim-neorg/neorg",
+    "kristijanhusak/orgmode.nvim",
     config = function()
-      require("neorg").setup {
-        -- Tell Neorg what modules to load
-        load = {
-          ["core.defaults"] = {}, -- Load all the default modules
-          ["core.norg.concealer"] = {}, -- Allows for use of icons
-          ["core.norg.dirman"] = { -- Manage your directories with Neorg
-            config = {
-              workspaces = {
-                my_workspace = "~/neorg",
-              },
-            },
-          },
-          ["core.norg.completion"] = {
-            config = {
-              engine = "nvim-cmp", -- We current support nvim-compe and nvim-cmp only
-            },
-          },
-        },
+      require("orgmode").setup {
+        org_agenda_files = { "~/org/agenda/**/*" },
+        org_default_notes_file = "~/org/notes/refile.org",
       }
     end,
-    requires = "nvim-lua/plenary.nvim",
   },
+  {
+    "wellle/targets.vim",
+  },
+  -- {
+  --   "nvim-neorg/neorg",
+  --   config = function()
+  --     require("neorg").setup {
+  --       -- Tell Neorg what modules to load
+  --       load = {
+  --         ["core.defaults"] = {}, -- Load all the default modules
+  --         ["core.keybinds"] = { -- Configure core.keybinds
+  --           config = {
+  --             default_keybinds = true, -- Generate the default keybinds
+  --             neorg_leader = "<Leader>o", -- This is the default if unspecified
+  --           },
+  --         },
+  --         ["core.norg.concealer"] = {}, -- Allows for use of icons
+  --         ["core.norg.dirman"] = { -- Manage your directories with Neorg
+  --           config = {
+  --             workspaces = {
+  --               my_workspace = "~/neorg",
+  --             },
+  --           },
+  --         },
+  --         ["core.integrations.telescope"] = {},
+  --         ["core.norg.completion"] = {
+  --           config = {
+  --             engine = "nvim-cmp", -- We current support nvim-compe and nvim-cmp only
+  --           },
+  --         },
+  --       },
+  --     }
+  --   end,
+  --   requires = "vhyrro/neorg-telescope",
+  -- },
   -- {
   --   "tom-doerr/vim_codex",
   -- },
