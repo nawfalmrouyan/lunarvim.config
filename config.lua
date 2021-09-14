83b321134e01d36a09bb023730b750ebbdfb2fc2
Nawfal <nawfalmrouyan@gmail.com>
2021-09-14 22:32:47 +0800

    enable neorg & disable orgmode. Looks like neorg is better for me right now

--- config.lua
+++ config.lua
@@ -341,50 +341,50 @@ lvim.plugins = {
     end,
     event = "BufEnter",
   },
-  {
-    "kristijanhusak/orgmode.nvim",
-    config = function()
-      require("orgmode").setup {
-        org_agenda_files = { "~/org/agenda/**/*" },
-        org_default_notes_file = "~/org/notes/refile.org",
-      }
-    end,
-  },
+  -- {
+  --   "kristijanhusak/orgmode.nvim",
+  --   config = function()
+  --     require("orgmode").setup {
+  --       org_agenda_files = { "~/org/agenda/**/*" },
+  --       org_default_notes_file = "~/org/notes/refile.org",
+  --     }
+  --   end,
+  -- },
   {
     "wellle/targets.vim",
   },
-  -- {
-  --   "nvim-neorg/neorg",
-  --   config = function()
-  --     require("neorg").setup {
-  --       -- Tell Neorg what modules to load
-  --       load = {
-  --         ["core.defaults"] = {}, -- Load all the default modules
-  --         ["core.keybinds"] = { -- Configure core.keybinds
-  --           config = {
-  --             default_keybinds = true, -- Generate the default keybinds
-  --             neorg_leader = "<Leader>o", -- This is the default if unspecified
-  --           },
-  --         },
-  --         ["core.norg.concealer"] = {}, -- Allows for use of icons
-  --         ["core.norg.dirman"] = { -- Manage your directories with Neorg
-  --           config = {
-  --             workspaces = {
-  --               my_workspace = "~/neorg",
-  --             },
-  --           },
-  --         },
-  --         ["core.integrations.telescope"] = {},
-  --         ["core.norg.completion"] = {
-  --           config = {
-  --             engine = "nvim-cmp", -- We current support nvim-compe and nvim-cmp only
-  --           },
-  --         },
-  --       },
-  --     }
-  --   end,
-  --   requires = "vhyrro/neorg-telescope",
-  -- },
+  {
+    "nvim-neorg/neorg",
+    config = function()
+      require("neorg").setup {
+        -- Tell Neorg what modules to load
+        load = {
+          ["core.defaults"] = {}, -- Load all the default modules
+          ["core.keybinds"] = { -- Configure core.keybinds
+            config = {
+              default_keybinds = true, -- Generate the default keybinds
+              neorg_leader = "<Leader>o", -- This is the default if unspecified
+            },
+          },
+          ["core.norg.concealer"] = {}, -- Allows for use of icons
+          ["core.norg.dirman"] = { -- Manage your directories with Neorg
+            config = {
+              workspaces = {
+                my_workspace = "~/neorg",
+              },
+            },
+          },
+          ["core.integrations.telescope"] = {},
+          ["core.norg.completion"] = {
+            config = {
+              engine = "nvim-cmp", -- We current support nvim-compe and nvim-cmp only
+            },
+          },
+        },
+      }
+    end,
+    requires = "vhyrro/neorg-telescope",
+  },
   -- {
   --   "tom-doerr/vim_codex",
   -- },
