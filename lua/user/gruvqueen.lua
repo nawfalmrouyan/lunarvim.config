b0e47b47e06dff1206f1fd86eaa203eedc5c3656
Nawfal <nawfalmrouyan@gmail.com>
2021-09-20 00:49:25 +0800

    gruvqueen config file

---
+++ lua/user/gruvqueen.lua
@@ -0,0 +1,14 @@
+vim.o.background = "dark" -- or light if you so prefer
+require("gruvqueen").setup {
+  config = {
+    disable_bold = true,
+    italic_comments = true,
+    italic_keywords = true,
+    italic_functions = true,
+    italic_variables = true,
+    invert_selection = false,
+    style = "mix", -- possible values: 'original', 'mix', 'material'
+    -- transparent_background = true,
+    -- bg_color = "black",
+  },
+}
