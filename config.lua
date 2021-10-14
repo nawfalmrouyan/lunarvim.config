342b5f4b226ac36af4a9175a99d420e3d12ee7de
Nawfal <nawfalmrouyan@gmail.com>
2021-10-14 12:30:32 +0800

    enable tokyonight theme

--- config.lua
+++ config.lua
@@ -372,13 +372,13 @@ lvim.plugins = {
       require "user.gruvbox-flat"
     end,
   },
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require "user.tokyonight"
-  --   end,
-  -- },
+  {
+    "folke/tokyonight.nvim",
+    event = "BufRead",
+    config = function()
+      require "user.tokyonight"
+    end,
+  },
   {
     "nvim-telescope/telescope-fzy-native.nvim",
     run = "make",
@@ -487,3 +487,26 @@ vim.cmd "autocmd VimLeave,VimSuspend * set guicursor=a:hor20"
 --       And I think `ver` means vertical and `hor` means horizontal.
 --       The numbers didn't make a difference in alacritty. Please change
 --       the number to something that suits your needs if it looks weird.
+
+local lspconfig = require'lspconfig'
+local configs = require'lspconfig/configs'
+local util = require 'lspconfig/util'
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+if not lspconfig.emmet_language_server then
+  configs.emmet_language_server = {
+    default_config = {
+      cmd = {'emmet-language-server', '--stdio'};
+      filetypes = {
+          'html', 'typescriptreact', 'javascriptreact', 'javascript',
+          'typescript', 'javascript.jsx', 'typescript.tsx', 'css'
+      },
+      root_dir = util.root_pattern("package.json", ".git"),
+      settings = {};
+    };
+  }
+end
+lspconfig.emmet_language_server.setup{ capabilities = capabilities; }
+
