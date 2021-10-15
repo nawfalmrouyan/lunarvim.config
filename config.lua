61bf8843e7cd38f23e2c244e6ddad46ca898f09f
Nawfal <nawfalmrouyan@gmail.com>
2021-10-15 15:35:34 +0800

    disable emmet_language_server

--- config.lua
+++ config.lua
@@ -488,25 +488,25 @@ vim.cmd "autocmd VimLeave,VimSuspend * set guicursor=a:hor20"
 --       The numbers didn't make a difference in alacritty. Please change
 --       the number to something that suits your needs if it looks weird.
 
-local lspconfig = require'lspconfig'
-local configs = require'lspconfig/configs'
-local util = require 'lspconfig/util'
+-- local lspconfig = require'lspconfig'
+-- local configs = require'lspconfig/configs'
+-- local util = require 'lspconfig/util'
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-if not lspconfig.emmet_language_server then
-  configs.emmet_language_server = {
-    default_config = {
-      cmd = {'emmet-language-server', '--stdio'};
-      filetypes = {
-          'html', 'typescriptreact', 'javascriptreact', 'javascript',
-          'typescript', 'javascript.jsx', 'typescript.tsx', 'css'
-      },
-      root_dir = util.root_pattern("package.json", ".git"),
-      settings = {};
-    };
-  }
-end
-lspconfig.emmet_language_server.setup{ capabilities = capabilities; }
+-- if not lspconfig.emmet_language_server then
+--   configs.emmet_language_server = {
+--     default_config = {
+--       cmd = {'emmet-language-server', '--stdio'};
+--       filetypes = {
+--           'html', 'typescriptreact', 'javascriptreact', 'javascript',
+--           'typescript', 'javascript.jsx', 'typescript.tsx', 'css'
+--       },
+--       root_dir = util.root_pattern("package.json", ".git"),
+--       settings = {};
+--     };
+--   }
+-- end
+-- lspconfig.emmet_language_server.setup{ capabilities = capabilities; }
 
