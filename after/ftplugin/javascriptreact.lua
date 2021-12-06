6a9d85ce01772d84a766f3a9e7b18414780e9e03
Nawfal <nawfalmrouyan@gmail.com>
2021-12-06 10:10:21 +0800

    update using ls_emmet

--- after/ftplugin/javascriptreact.lua
+++ after/ftplugin/javascriptreact.lua
@@ -3,3 +3,25 @@ local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup { { exe = "prettier" } }
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup { { exe = "eslint_d" } }
+
+local lspconfig = require'lspconfig'
+local configs = require'lspconfig.configs'
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+if not configs.ls_emmet then
+  configs.ls_emmet = {
+    default_config = {
+      cmd = { 'ls_emmet', '--stdio' };
+      filetypes = { 'html', 'css', 'scss', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'haml',
+        'xml', 'xsl', 'pug', 'slim', 'sass', 'stylus', 'less', 'sss'};
+      root_dir = function(fname)
+        return vim.loop.cwd()
+      end;
+      settings = {};
+    };
+  }
+end
+
+lspconfig.ls_emmet.setup{ capabilities = capabilities }
--- after/ftplugin/typescriptreact.lua
+++ after/ftplugin/typescriptreact.lua
@@ -5,23 +5,23 @@ local linters = require "lvim.lsp.null-ls.linters"
 linters.setup { { exe = "eslint_d" } }
 
 local lspconfig = require'lspconfig'
-local configs = require'lspconfig/configs'
-local util = require 'lspconfig/util'
+local configs = require'lspconfig.configs'
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-if not lspconfig.emmet_language_server then
-  configs.emmet_language_server = {
+if not configs.ls_emmet then
+  configs.ls_emmet = {
     default_config = {
-      cmd = {'emmet-language-server', '--stdio'};
-      filetypes = {
-          'html', 'typescriptreact', 'javascriptreact', 'javascript',
-          'typescript', 'javascript.jsx', 'typescript.tsx', 'css'
-      },
-      root_dir = util.root_pattern("package.json", ".git"),
+      cmd = { 'ls_emmet', '--stdio' };
+      filetypes = { 'html', 'css', 'scss', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'haml',
+        'xml', 'xsl', 'pug', 'slim', 'sass', 'stylus', 'less', 'sss'};
+      root_dir = function(fname)
+        return vim.loop.cwd()
+      end;
       settings = {};
     };
   }
 end
-lspconfig.emmet_language_server.setup{ capabilities = capabilities; }
+
+lspconfig.ls_emmet.setup{ capabilities = capabilities }
