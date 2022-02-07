7f1786a5717f551782d8de9c4ea100d9dee851fe
Nawfal <nawfalmrouyan@gmail.com>
2022-02-07 10:12:02 +0800

    update table override, include zk

--- config.lua
+++ config.lua
@@ -85,8 +85,9 @@ lvim.builtin.treesitter.textobjects = {
 }
 
 -- Remove tailwindcss from lvim.lsp.override table
-table.remove(lvim.lsp.override, 29)
-table.remove(lvim.lsp.override, 29)
+table.remove(lvim.lsp.override, 30)
+table.remove(lvim.lsp.override, 30)
+table.remove(lvim.lsp.override, 33)
 
 -- Personal Keymaps
 lvim.keys.insert_mode["<M-o>"] = "<C-o>o"
