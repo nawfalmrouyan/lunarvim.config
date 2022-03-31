d4bd1e8547921aa78b9f5c55d016fa0bb5e69ec6
Nawfal <nawfalmrouyan@gmail.com>
2022-03-31 14:07:26 +0800

    in case we want to turn off automatic lsp server installation

--- config.lua
+++ config.lua
@@ -32,6 +32,7 @@ lvim.builtin.terminal.shading_factor = 1
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.notify.opts.timeout = 3000
 
+lvim.lsp.automatic_servers_installation = true
 lvim.lsp.diagnostics.virtual_text = false
 
 lvim.builtin.treesitter.ensure_installed = "maintained"
