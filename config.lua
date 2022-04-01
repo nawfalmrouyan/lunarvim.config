b99fec55bd071fc956ec5b9037f14b6e0c4f4ff6
Nawfal <nawfalmrouyan@gmail.com>
2022-04-01 19:08:52 +0800

    not needed to detect terraform ft

--- config.lua
+++ config.lua
@@ -498,9 +498,3 @@ vim.opt.fillchars = {
   vertright = "┣",
   verthoriz = "╋",
 }
-
-local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
-parser_configs.hcl = {
-  filetype = "hcl",
-  "terraform",
-}
