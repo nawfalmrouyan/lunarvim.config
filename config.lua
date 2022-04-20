ffd45a854c5337d7b46613627b56e3b903177a35
Nawfal <nawfalmrouyan@gmail.com>
2022-04-20 10:32:05 +0800

    specs.nvim plugin

--- config.lua
+++ config.lua
@@ -412,6 +412,32 @@ lvim.plugins = {
       }
     end,
   },
+  {
+    "edluffy/specs.nvim",
+    config = function()
+      require("specs").setup {
+        show_jumps = true,
+        min_jump = 30,
+        popup = {
+          delay_ms = 0, -- delay before popup displays
+          inc_ms = 9, -- time increments used for fade/resize effects
+          blend = 70, -- starting blend, between 0-100 (fully transparent), see :h winblend
+          width = 50,
+          winhl = "PMenu",
+          fader = require("specs").linear_fader,
+          resizer = require("specs").shrink_resizer,
+        },
+        ignore_filetypes = {},
+        ignore_buftypes = {
+          nofile = true,
+        },
+      }
+      lvim.builtin.which_key.mappings["d"] = { "<cmd>lua require('specs').show_specs()<CR>", "Show cursor position" }
+      -- You can even bind it to search jumping and more, example:
+      vim.api.nvim_set_keymap("n", "n", 'n:lua require("specs").show_specs()<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "N", 'N:lua require("specs").show_specs()<CR>', { noremap = true, silent = true })
+    end,
+  },
 }
 
 lvim.autocommands.custom_groups = {
