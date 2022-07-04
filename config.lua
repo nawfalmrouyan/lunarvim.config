31cc9166cec258d0de5e038db1b7e4b6333e1611
Nawfal <nawfalmrouyan@gmail.com>
2022-07-04 11:29:25 +0800

    remove fold options configs. will use config in plugins later

--- config.lua
+++ config.lua
@@ -17,7 +17,7 @@ vim.opt.scrolloff = 0
 vim.opt.sidescrolloff = 0
 vim.opt.inccommand = "split"
 -- vim.cmd "set foldmethod=manual"
-vim.cmd "set foldexpr=nvim_treesitter#foldexpr()"
+-- vim.cmd "set foldexpr=nvim_treesitter#foldexpr()"
 vim.go.laststatus = 3
 
 lvim.builtin.lualine.active = true
