2abc2e08d389a2d6df9eee1bebbd214048fcd829
Nawfal <nawfalmrouyan@gmail.com>
2022-08-01 17:39:51 +0800

    use lspsaga

--- config.lua
+++ config.lua
@@ -466,6 +466,39 @@ lvim.plugins = {
       }
     end,
   },
+  {
+    "glepnir/lspsaga.nvim",
+    branch = "main",
+    config = function()
+      local saga = require "lspsaga"
+      saga.init_lsp_saga()
+      -- lsp finder to find the cursor word definition and reference
+      vim.keymap.set("n", "gh", require("lspsaga.finder").lsp_finder, { silent = true, noremap = true })
+
+      -- jump and show diagnostics
+      vim.keymap.set(
+        "n",
+        "<leader>cd",
+        require("lspsaga.diagnostic").show_line_diagnostics,
+        { silent = true, noremap = true }
+      )
+      -- jump diagnostic
+      vim.keymap.set("n", "[e", require("lspsaga.diagnostic").goto_prev, { silent = true, noremap = true })
+      vim.keymap.set("n", "]e", require("lspsaga.diagnostic").goto_next, { silent = true, noremap = true })
+      -- or jump to error
+      vim.keymap.set("n", "[E", function()
+        require("lspsaga.diagnostic").goto_prev { severity = vim.diagnostic.severity.ERROR }
+      end, { silent = true, noremap = true })
+      vim.keymap.set("n", "]E", function()
+        require("lspsaga.diagnostic").goto_next { severity = vim.diagnostic.severity.ERROR }
+      end, { silent = true, noremap = true })
+
+      -- show signature help
+      vim.keymap.set("n", "gs", require("lspsaga.signaturehelp").signature_help, { silent = true, noremap = true })
+
+      lvim.builtin.which_key.mappings.l.o = { "<CMD>LSoutlineToggle<cr>", "Outline" }
+    end,
+  },
 }
 
 lvim.autocommands = {
