8e401166410e3f28bd315a7392ebb5f1ef43b335
Nawfal <nawfalmrouyan@gmail.com>
2022-08-25 16:08:02 +0800

    use pcall

--- lua/user/dial.lua
+++ lua/user/dial.lua
@@ -2,7 +2,11 @@ local M = {}
 
 M.config = function()
 
-  local augend = require "dial.augend"
+  local status_ok, augend = pcall(require, "dial.augend")
+  if not status_ok then
+    return
+  end
+
   require("dial.config").augends:register_group {
     default = {
       augend.integer.alias.decimal,
