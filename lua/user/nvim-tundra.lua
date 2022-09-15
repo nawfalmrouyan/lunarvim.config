10df9a7784c56c3bec5cb6e9dee873871744fc20
Nawfal <nawfalmrouyan@gmail.com>
2022-09-15 16:20:36 +0800

    in case we want to use tundra colorscheme

---
+++ lua/user/nvim-tundra.lua
@@ -0,0 +1,52 @@
+local M = {}
+
+M.config = function()
+  local status_ok, tundra = pcall(require, "nvim-tundra")
+  if not status_ok then
+    return
+  end
+  tundra.setup {
+    transparent_background = false,
+    editor = {
+      search = {},
+      substitute = {},
+    },
+    syntax = {
+      booleans = { bold = true, italic = true },
+      comments = { bold = true, italic = true },
+      conditionals = {},
+      constants = { bold = true },
+      functions = {},
+      keywords = {},
+      loops = {},
+      numbers = { bold = true },
+      operators = { bold = true },
+      punctuation = {},
+      strings = {},
+      types = { italic = true },
+    },
+    diagnostics = {
+      errors = {},
+      warnings = {},
+      information = {},
+      hints = {},
+    },
+    plugins = {
+      lsp = true,
+      treesitter = true,
+      cmp = true,
+      context = true,
+      dbui = true,
+      gitsigns = true,
+      telescope = true,
+    },
+    overwrite = {
+      colors = {},
+      highlights = {},
+    },
+  }
+
+  vim.opt.background = "dark"
+end
+
+return M
