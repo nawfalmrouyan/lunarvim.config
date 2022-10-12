a3015e5574a57ea2050feeaa3251c4d721ba80d8
Nawfal <nawfalmrouyan@gmail.com>
2022-10-12 12:17:05 +0800

    update catppuccin integrations config

--- lua/user/catppuccin.lua
+++ lua/user/catppuccin.lua
@@ -32,68 +32,73 @@ M.config = function()
       types = { "italic" },
       operators = {},
     },
-    integrations = {
-      treesitter = true,
-      native_lsp = {
-        enabled = true,
-        virtual_text = {
-          errors = { "italic" },
-          hints = { "italic" },
-          warnings = { "italic" },
-          information = { "italic" },
+    require("catppuccin").setup {
+      integrations = {
+        aerial = false,
+        barbar = true,
+        beacon = false,
+        cmp = true,
+        coc_nvim = false,
+        dashboard = true,
+        fern = false,
+        fidget = false,
+        gitgutter = false,
+        gitsigns = true,
+        harpoon = false,
+        hop = false,
+        illuminate = true,
+        leap = true,
+        lightspeed = false,
+        lsp_saga = true,
+        lsp_trouble = false,
+        markdown = true,
+        mini = false,
+        neogit = false,
+        neotest = false,
+        neotree = false,
+        notify = true,
+        nvimtree = true,
+        overseer = false,
+        pounce = false,
+        symbols_outline = false,
+        telekasten = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = false,
+        ts_rainbow = true,
+        vim_sneak = false,
+        vimwiki = false,
+        which_key = true,
+
+        -- Special integrations, see https://github.com/catppuccin/nvim#special-integrations
+        dap = {
+          enabled = false,
+          enable_ui = false,
         },
-        underlines = {
-          errors = { "underline" },
-          hints = { "underline" },
-          warnings = { "underline" },
-          information = { "underline" },
+        indent_blankline = {
+          enabled = true,
+          colored_indent_levels = false,
+        },
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+          },
+        },
+        navic = {
+          enabled = true,
+          custom_bg = "NONE",
         },
       },
-      coc_nvim = false,
-      lsp_trouble = true,
-      cmp = true,
-      lsp_saga = true,
-      gitgutter = false,
-      gitsigns = true,
-      leap = true,
-      telescope = true,
-      nvimtree = {
-        enabled = true,
-        show_root = true,
-        transparent_panel = false,
-      },
-      neotree = {
-        enabled = false,
-        show_root = true,
-        transparent_panel = false,
-      },
-      dap = {
-        enabled = false,
-        enable_ui = false,
-      },
-      which_key = true,
-      indent_blankline = {
-        enabled = true,
-        colored_indent_levels = false,
-      },
-      dashboard = true,
-      neogit = false,
-      vim_sneak = false,
-      fern = false,
-      barbar = false,
-      markdown = true,
-      lightspeed = true,
-      ts_rainbow = true,
-      hop = false,
-      notify = true,
-      telekasten = false,
-      symbols_outline = true,
-      mini = false,
-      aerial = false,
-      vimwiki = false,
-      beacon = false,
-      navic = false,
-      overseer = false,
     },
     color_overrides = {},
     highlight_overrides = {},
@@ -105,15 +110,15 @@ M.config = function()
     end,
   })
   -- Create an autocmd User PackerCompileDone to update it every time packer is compiled
-  vim.api.nvim_create_autocmd("User", {
-    pattern = "PackerCompileDone",
-    callback = function()
-      vim.cmd "CatppuccinCompile"
-      vim.defer_fn(function()
-        vim.cmd "colorscheme catppuccin"
-      end, 0) -- Defered for live reloading
-    end,
-  })
+  -- vim.api.nvim_create_autocmd("User", {
+  --   pattern = "PackerCompileDone",
+  --   callback = function()
+  --     vim.cmd "CatppuccinCompile"
+  --     vim.defer_fn(function()
+  --       vim.cmd "colorscheme catppuccin"
+  --     end, 0) -- Defered for live reloading
+  --   end,
+  -- })
   vim.cmd [[colorscheme catppuccin]]
 end
 
