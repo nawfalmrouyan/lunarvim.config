ecd693aaae7b41a9a7d33c04fdf530bdd7fc2078
Nawfal <nawfalmrouyan@gmail.com>
2022-12-03 00:23:10 +0800

    disable cmp signature plugin

--- config.lua
+++ config.lua
@@ -310,6 +310,12 @@ lvim.plugins = {
       require("cmp").setup.cmdline("/", { sources = { { name = "buffer" } } })
     end,
   },
+  -- {
+  --   "hrsh7th/cmp-nvim-lsp-signature-help",
+  --   config = function()
+  --     require("cmp").setup { sources = { { name = "nvim_lsp_signature_help" } } }
+  --   end,
+  -- },
   {
     "mbbill/undotree",
     event = "BufRead",
