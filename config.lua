524ec58c0fc2197a1afac6c20c65fb0152c6d028
Nawfal <nawfalmrouyan@gmail.com>
2022-12-16 07:46:08 +0800

    lol... no wonder it wasn't working.... adooii

--- config.lua
+++ config.lua
@@ -158,9 +158,9 @@ lvim.plugins = {
   {
     "ray-x/lsp_signature.nvim",
     event = "BufRead",
-    -- config = function()
-    --   require("lsp_signature").on_attach()
-    -- end,
+    config = function()
+      require("lsp_signature").setup()
+    end,
   },
   -- {
   --   "glacambre/firenvim",
