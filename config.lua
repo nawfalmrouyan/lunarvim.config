298381659498d086ead1e61d653232739d8255d0
Nawfal <nawfalmrouyan@gmail.com>
2022-12-16 14:59:56 +0800

    lf plugin

--- config.lua
+++ config.lua
@@ -157,6 +157,23 @@ lvim.builtin.sell_soul_to_devel = true
 -- Additional Plugins
 lvim.plugins = {
   {
+    "lmburns/lf.nvim",
+    config = function()
+      -- This feature will not work if the plugin is lazy-loaded
+      vim.g.lf_netrw = 1
+
+      require("lf").setup {
+        escape_quit = false,
+        border = "rounded",
+        highlights = { FloatBorder = { guifg = "#819c3b" } },
+        winblend = 0,
+      }
+
+      vim.keymap.set("n", "<C-o>", ":Lf<CR>")
+    end,
+    requires = { "plenary.nvim", "toggleterm.nvim" },
+  },
+  {
     "ray-x/lsp_signature.nvim",
     event = "BufRead",
     config = function()
