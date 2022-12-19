d9e31b846be6538f7a6e3ed071adb07ac1cd1c1b
Nawfal <nawfalmrouyan@gmail.com>
2022-12-19 19:22:36 +0800

    next time fzf-lua. defaults back to user telescope

--- config.lua
+++ config.lua
@@ -332,30 +332,30 @@ lvim.plugins = {
   { "mg979/vim-visual-multi", event = "BufEnter" }, -- vim -Nu ~/.local/share/lunarvim/site/pack/packer/start/vim-visual-multi/tutorialrc
   { "nvim-treesitter/nvim-treesitter-textobjects", before = "nvim-treesitter" },
   { "mzlogin/vim-markdown-toc", event = "BufRead" },
-  {
-    "ibhagwan/fzf-lua",
-    event = "BufRead",
-    requires = { "kyazdani42/nvim-web-devicons" },
-    setup = function()
-      vim.api.nvim_set_keymap(
-        "n",
-        "<c-P>",
-        "<cmd>lua require('fzf-lua').files()<cr>",
-        { noremap = true, silent = true }
-      )
-      vim.api.nvim_set_keymap("n", "<c-0>", ":FzfLua ", { noremap = true, silent = false })
-      lvim.builtin.which_key.mappings["f"] = {
-        name = "FZF",
-        c = { "<cmd>lua require('fzf-lua').grep_cword()<cr>", "Find cword" },
-        C = { "<cmd>lua require('fzf-lua').grep_cWORD()<cr>", "Find cWORD" },
-        g = { "<cmd>lua require('fzf-lua').live_grep()<cr>", "Live grep" },
-        G = { "<cmd>lua require('fzf-lua').live_grep_resume()<cr>", "Live grep resume" },
-        f = { "<cmd>lua require('fzf-lua').files()<cr>", "Find files" },
-        r = { "<cmd>lua require('fzf-lua').resume()<cr>", "Resume last search" },
-        t = { "<cmd>lua require('fzf-lua').tabs()<cr>", "Resume last search" },
-      }
-    end,
-  },
+  -- {
+  --   "ibhagwan/fzf-lua",
+  --   event = "BufRead",
+  --   requires = { "kyazdani42/nvim-web-devicons" },
+  --   setup = function()
+  --     vim.api.nvim_set_keymap(
+  --       "n",
+  --       "<c-P>",
+  --       "<cmd>lua require('fzf-lua').files()<cr>",
+  --       { noremap = true, silent = true }
+  --     )
+  --     vim.api.nvim_set_keymap("n", "<c-0>", ":FzfLua ", { noremap = true, silent = false })
+  --     lvim.builtin.which_key.mappings["f"] = {
+  --       name = "FZF",
+  --       c = { "<cmd>lua require('fzf-lua').grep_cword()<cr>", "Find cword" },
+  --       C = { "<cmd>lua require('fzf-lua').grep_cWORD()<cr>", "Find cWORD" },
+  --       g = { "<cmd>lua require('fzf-lua').live_grep()<cr>", "Live grep" },
+  --       G = { "<cmd>lua require('fzf-lua').live_grep_resume()<cr>", "Live grep resume" },
+  --       f = { "<cmd>lua require('fzf-lua').files()<cr>", "Find files" },
+  --       r = { "<cmd>lua require('fzf-lua').resume()<cr>", "Resume last search" },
+  --       t = { "<cmd>lua require('fzf-lua').tabs()<cr>", "Resume last search" },
+  --     }
+  --   end,
+  -- },
   {
     "kylechui/nvim-surround",
     event = "BufEnter",
