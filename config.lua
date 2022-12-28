8d3bb71643b815eb94069865c195679dc233250b
Nawfal <nawfalmrouyan@gmail.com>
2022-12-28 20:06:53 +0800

    feat(plugin): tabnine plugin.

--- config.lua
+++ config.lua
@@ -198,6 +198,27 @@ lvim.builtin.sell_soul_to_devel = true
 -- Additional Plugins
 lvim.plugins = {
   {
+    "tzachar/cmp-tabnine",
+    build = "./install.sh",
+    dependencies = "hrsh7th/nvim-cmp",
+    -- event = "InsertEnter",
+    config = function()
+      local tabnine = require "cmp_tabnine.config"
+
+      tabnine:setup {
+        max_lines = 1000,
+        max_num_results = 20,
+        sort = true,
+        run_on_every_keystroke = true,
+        snippet_placeholder = "..",
+        ignored_file_types = {
+          -- lua = true
+        },
+        show_prediction_strength = false,
+      }
+    end,
+  },
+  {
     "chrisgrieser/nvim-various-textobjs",
     event = "BufRead",
     config = function()
