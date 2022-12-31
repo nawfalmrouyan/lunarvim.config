d3647d0531d29f84a5b685603c5c58bf7cd3e9df
Nawfal <nawfalmrouyan@gmail.com>
2023-01-01 01:49:52 +0800

    replace nvim-surround with mini-surround and install mini.ai

--- config.lua
+++ config.lua
@@ -471,25 +471,98 @@ lvim.plugins = {
   --   end,
   -- },
   {
-    "kylechui/nvim-surround",
+    "echasnovski/mini.ai",
     event = "VeryLazy",
     config = function()
-      require("nvim-surround").setup {
-        keymaps = {
-          insert = "<C-g>z",
-          insert_line = "gC-ggZ",
-          normal = "gz",
-          normal_cur = "gZ",
-          normal_line = "gzz",
-          normal_cur_line = "gZZ",
-          visual = "gz",
-          visual_line = "gZ",
-          delete = "gzd",
-          change = "gzr",
+      require("mini.ai").setup {
+        -- Table with textobject id as fields, textobject specification as values.
+        -- Also use this to disable builtin textobjects. See |MiniAi.config|.
+        custom_textobjects = nil,
+
+        -- Module mappings. Use `''` (empty string) to disable one.
+        mappings = {
+          -- Main textobject prefixes
+          around = "a",
+          inside = "i",
+
+          -- Next/last variants
+          around_next = "an",
+          inside_next = "in",
+          around_last = "al",
+          inside_last = "il",
+
+          -- Move cursor to corresponding edge of `a` textobject
+          goto_left = "g[",
+          goto_right = "g]",
         },
+
+        -- Number of lines within which textobject is searched
+        n_lines = 50,
+
+        -- How to search for object (first inside current line, then inside
+        -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+        -- 'cover_or_nearest', 'next', 'previous', 'nearest'.
+        search_method = "cover_or_next",
       }
     end,
   },
+  {
+    "echasnovski/mini.surround",
+    event = "VeryLazy",
+    config = function()
+      require("mini.surround").setup {
+        -- Add custom surroundings to be used on top of builtin ones. For more
+        -- information with examples, see `:h MiniSurround.config`.
+        custom_surroundings = nil,
+
+        -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
+        highlight_duration = 500,
+
+        -- Module mappings. Use `''` (empty string) to disable one.
+        mappings = {
+          add = "gza", -- Add surrounding in Normal and Visual modes
+          delete = "gzd", -- Delete surrounding
+          find = "gzf", -- Find surrounding (to the right)
+          find_left = "gzF", -- Find surrounding (to the left)
+          highlight = "gzh", -- Highlight surrounding
+          replace = "gzr", -- Replace surrounding
+          update_n_lines = "gzn", -- Update `n_lines`
+
+          suffix_last = "l", -- Suffix to search with "prev" method
+          suffix_next = "n", -- Suffix to search with "next" method
+        },
+
+        -- Number of lines within which surrounding is searched
+        n_lines = 20,
+
+        -- How to search for surrounding (first inside current line, then inside
+        -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+        -- 'cover_or_nearest', 'next', 'prev', 'nearest'. For more details,
+        -- see `:h MiniSurround.config`.
+        search_method = "cover",
+      }
+    end,
+  },
+  -- {
+  --   "kylechui/nvim-surround",
+  --   event = "VeryLazy",
+  --   config = function()
+  --     require("nvim-surround").setup {
+  --       keymaps = {
+  --         insert = "<C-g>z",
+  --         insert_line = "gC-ggZ",
+  --         normal = "gz",
+  --         normal_cur = "gZ",
+  --         normal_line = "gzz",
+  --         normal_cur_line = "gZZ",
+  --         visual = "gz",
+  --         visual_line = "gZ",
+  --         delete = "gzd",
+  --         change = "gzr",
+  --       },
+  --     }
+  --   end,
+  -- },
   -- {
   --   "kevinhwang91/nvim-ufo",
   --   event = "BufRead",
