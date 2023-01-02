235a79b68972a065bf4d64c1643e2794cf253083
Nawfal <nawfalmrouyan@gmail.com>
2023-01-02 23:38:25 +0800

    feat(plugins): replace aerial with symbols outline

--- config.lua
+++ config.lua
@@ -353,34 +353,7 @@ lvim.plugins = {
     "romgrk/nvim-treesitter-context",
     event = "VeryLazy",
     config = function()
-      require("treesitter-context").setup {
-        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-        throttle = true, -- Throttles plugin updates (may improve performance)
-        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-        patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-          -- For all filetypes
-          -- Note that setting an entry here replaces all other patterns for this entry.
-          -- By setting the 'default' entry below, you can control which nodes you want to
-          -- appear in the context window.
-          default = {
-            "class",
-            "function",
-            "method",
-          },
-        },
-      }
-    end,
-  },
-  {
-    "stevearc/aerial.nvim",
-    event = "VeryLazy",
-    init = function()
-      lvim.builtin.which_key.mappings.l.o = { "<cmd>AerialToggle!<cr>", "Outline" }
-    end,
-    config = function()
-      require("aerial").setup {
-        show_guides = true,
-      }
+      require("user.treesitter-context").config()
     end,
   },
   {
