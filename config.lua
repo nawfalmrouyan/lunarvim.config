570a1ef7ec85a198f05cd7623b8c36eaf856c5df
Nawfal <nawfalmrouyan@gmail.com>
2023-01-13 18:55:46 +0800

    fix: symbols-outline color

--- config.lua
+++ config.lua
@@ -217,17 +217,46 @@ lvim.plugins = {
     end,
     dependencies = { "nvim-neorg/neorg-telescope", "nvim-lua/plenary.nvim" },
   },
-  -- {
-  --   "simrat39/symbols-outline.nvim",
-  --   event = "VeryLazy",
-  --   init = function()
-  --     lvim.builtin.which_key.mappings.l.o = { "<cmd>SymbolsOutline<cr>", "Outline" }
-  --   end,
-  --   config = function()
-  --     -- require("user.symbolsoutline").config()
-  --     require("symbols-outline").setup()
-  --   end,
-  -- },
+  {
+    "simrat39/symbols-outline.nvim",
+    event = "VeryLazy",
+    init = function()
+      lvim.builtin.which_key.mappings.l.o = { "<cmd>SymbolsOutline<cr>", "Outline" }
+    end,
+    config = function()
+      -- require("user.symbolsoutline").config()
+      require("symbols-outline").setup {
+        symbols = {
+          File = { hl = "@text.uri" },
+          Module = { hl = "@namespace" },
+          Namespace = { hl = "@namespace" },
+          Package = { hl = "@namespace" },
+          Class = { hl = "@type" },
+          Method = { hl = "@method" },
+          Property = { hl = "@method" },
+          Field = { hl = "@field" },
+          Constructor = { hl = "@constructor" },
+          Enum = { hl = "@type" },
+          Interface = { hl = "@type" },
+          Function = { hl = "@function" },
+          Variable = { hl = "@constant" },
+          Constant = { hl = "@constant" },
+          String = { hl = "@string" },
+          Number = { hl = "@number" },
+          Boolean = { hl = "@boolean" },
+          Array = { hl = "@constant" },
+          Object = { hl = "@type" },
+          Key = { hl = "@type" },
+          Null = { hl = "@type" },
+          EnumMember = { hl = "@field" },
+          Struct = { hl = "@type" },
+          Event = { hl = "@type" },
+          Operator = { hl = "@operator" },
+          TypeParameter = { hl = "@parameter" },
+        },
+      }
+    end,
+  },
   {
     "tzachar/cmp-tabnine",
     build = "./install.sh",
