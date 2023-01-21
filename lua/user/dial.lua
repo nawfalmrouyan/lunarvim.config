84f557d91713c3082d4e1d7a8f6edd7ad7cc1b16
Nawfal <nawfalmrouyan@gmail.com>
2023-01-21 11:20:39 +0800

    update augend aliases to match our need

--- lua/user/dial.lua
+++ lua/user/dial.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 M.config = function()
-
   local status_ok, augend = pcall(require, "dial.augend")
   if not status_ok then
     return
@@ -10,39 +9,29 @@ M.config = function()
   require("dial.config").augends:register_group {
     default = {
       augend.integer.alias.decimal,
-      augend.integer.alias.decimal_int,
+      -- augend.integer.alias.decimal_int,
       augend.integer.alias.hex,
       augend.integer.alias.binary,
       augend.integer.alias.octal,
-      augend.date.alias["%Y/%m/%d"],
+      augend.date.alias["%d/%m/%Y"],
+      augend.date.alias["%H:%M"],
       augend.constant.alias.bool,
       augend.constant.alias.alpha,
       augend.constant.alias.Alpha,
-      augend.hexcolor.new{
+      augend.hexcolor.new {
         case = "lower",
       },
       augend.semver.alias.semver,
-    },
-    visual = {
-      augend.integer.alias.decimal,
-      augend.integer.alias.hex,
-      augend.date.alias["%Y/%m/%d"],
-      augend.constant.alias.alpha,
-      augend.constant.alias.Alpha,
-    },
+    }
   }
 
   -- change augends in VISUAL mode
-  vim.api.nvim_set_keymap("v", "<C-a>", require("dial.map").inc_normal "visual", { noremap = true })
-  vim.api.nvim_set_keymap("v", "<C-x>", require("dial.map").dec_normal "visual", { noremap = true })
-
   vim.api.nvim_set_keymap("n", "<C-a>", require("dial.map").inc_normal(), { noremap = true })
   vim.api.nvim_set_keymap("n", "<C-x>", require("dial.map").dec_normal(), { noremap = true })
   vim.api.nvim_set_keymap("v", "<C-a>", require("dial.map").inc_visual(), { noremap = true })
   vim.api.nvim_set_keymap("v", "<C-x>", require("dial.map").dec_visual(), { noremap = true })
   vim.api.nvim_set_keymap("v", "g<C-a>", require("dial.map").inc_gvisual(), { noremap = true })
   vim.api.nvim_set_keymap("v", "g<C-x>", require("dial.map").dec_gvisual(), { noremap = true })
-
 end
 
 return M
