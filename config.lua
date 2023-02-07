026beba4f9580c343ce65159711ee1ef72b76eef
Nawfal <nawfalmrouyan@gmail.com>
2023-02-08 00:54:25 +0800

    feat(plugin): free copilot

--- config.lua
+++ config.lua
@@ -218,6 +218,25 @@ lvim.builtin.sell_soul_to_devel = true
 -- Additional Plugins
 lvim.plugins = {
   {
+    "Exafunction/codeium.vim",
+    event = "BufRead",
+    lazy = true,
+    init = function()
+      vim.keymap.set("i", "<C-g>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true })
+      vim.keymap.set("i", "<c-;>", function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end, { expr = true })
+      vim.keymap.set("i", "<c-,>", function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end, { expr = true })
+      vim.keymap.set("i", "<c-x>", function()
+        return vim.fn["codeium#Clear"]()
+      end, { expr = true })
+    end,
+  },
+  {
     "Aasim-A/scrollEOF.nvim",
     event = "BufRead",
     lazy = true,
