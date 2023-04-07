3553205966df776a7cca4c4aa82a00a49d837aa8
Nawfal <nawfalmrouyan@gmail.com>
2023-04-07 19:12:16 +0800

    schemastore already enabled by default in lunarvim

--- after/ftplugin/yaml.lua
+++
@@ -1,9 +0,0 @@
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "yamlls" })
-local opts = {
-  settings = {
-    yaml = {
-      schemas = require('schemastore').yaml.schemas(),
-    },
-  },
-}
-require("lvim.lsp.manager").setup("yamlls", opts)
