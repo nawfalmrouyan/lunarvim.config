608adce407829ac6858a45f342cd10d91d5b5ddc
Nawfal <nawfalmrouyan@gmail.com>
2023-07-20 04:26:06 +0800

    tag fidget to legacy (breaking changes incoming)

--- config.lua
+++ config.lua
@@ -810,6 +810,7 @@ lvim.plugins = {
   {
     "j-hui/fidget.nvim",
     event = "BufRead",
+    tag = "legacy",
     config = function()
       require("fidget").setup { window = { blend = 0 } }
     end,
