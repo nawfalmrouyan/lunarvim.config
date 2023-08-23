806923dc9777a7d1e506394ffd16f0028d7089c2
Nawfal <nawfalmrouyan@gmail.com>
2023-08-23 10:08:12 +0800

    feat: reenable mini.ai

--- config.lua
+++ config.lua
@@ -547,43 +547,42 @@ lvim.plugins = {
       vim.cmd "let g:VM_default_mappings = 0"
     end,
   }, -- vim -Nu ~/.local/share/lunarvim/site/pack/packer/start/vim-visual-multi/tutorialrc
-  -- {
-  --   "echasnovski/mini.ai",
-  --   enabled = false,
-  --   event = "BufRead",
-  --   config = function()
-  --     require("mini.ai").setup {
-  --       -- Table with textobject id as fields, textobject specification as values.
-  --       -- Also use this to disable builtin textobjects. See |MiniAi.config|.
-  --       custom_textobjects = nil,
+  {
+    "echasnovski/mini.ai",
+    event = "BufRead",
+    config = function()
+      require("mini.ai").setup {
+        -- Table with textobject id as fields, textobject specification as values.
+        -- Also use this to disable builtin textobjects. See |MiniAi.config|.
+        custom_textobjects = nil,
 
-  --       -- Module mappings. Use `''` (empty string) to disable one.
-  --       mappings = {
-  --         -- Main textobject prefixes
-  --         around = "a",
-  --         inside = "i",
+        -- Module mappings. Use `''` (empty string) to disable one.
+        mappings = {
+          -- Main textobject prefixes
+          around = "a",
+          inside = "i",
 
-  --         -- Next/last variants
-  --         around_next = "an",
-  --         inside_next = "in",
-  --         around_last = "al",
-  --         inside_last = "il",
+          -- Next/last variants
+          around_next = "an",
+          inside_next = "in",
+          around_last = "al",
+          inside_last = "il",
 
-  --         -- Move cursor to corresponding edge of `a` textobject
-  --         goto_left = "g[",
-  --         goto_right = "g]",
-  --       },
+          -- Move cursor to corresponding edge of `a` textobject
+          goto_left = "g[",
+          goto_right = "g]",
+        },
 
-  --       -- Number of lines within which textobject is searched
-  --       n_lines = 50,
+        -- Number of lines within which textobject is searched
+        n_lines = 50,
 
-  --       -- How to search for object (first inside current line, then inside
-  --       -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
-  --       -- 'cover_or_nearest', 'next', 'previous', 'nearest'.
-  --       search_method = "cover_or_nearest",
-  --     }
-  --   end,
-  -- },
+        -- How to search for object (first inside current line, then inside
+        -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+        -- 'cover_or_nearest', 'next', 'previous', 'nearest'.
+        search_method = "cover_or_nearest",
+      }
+    end,
+  },
   {
     "echasnovski/mini.move",
     event = "BufRead",
