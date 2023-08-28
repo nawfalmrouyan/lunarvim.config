4c2519ae5eef6e4b708cb139a475c1e71f314215
OpalBSD <nawfal@gmail.com>
2023-08-28 18:30:42 +0800

    move noice plugin config to a file

--- config.lua
+++ config.lua
@@ -283,25 +283,7 @@ lvim.plugins = {
   {
     "folke/noice.nvim",
     config = function()
-      require("noice").setup {
-        lsp = {
-          progress = { enabled = false },
-          signature = { enabled = false },
-          hover = { enabled = false },
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true,
-          },
-        },
-        presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
-          command_palette = true, -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = true, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = true, -- add a border to hover docs and signature help
-        },
-      }
+      require("user.noice").config()
     end,
     dependencies = { "MunifTanjim/nui.nvim" },
   },
