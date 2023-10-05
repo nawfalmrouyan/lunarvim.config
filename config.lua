8925ea060d96ae5ce51a05bf960b557d93878879
Nawfal <nawfalmrouyan@gmail.com>
2023-10-05 21:30:44 +0800

    add extra line

--- config.lua
+++ config.lua
@@ -28,6 +28,7 @@ vim.opt.colorcolumn = "100"
 -- test highlight only line number.
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = "number"
+
 -- #22668 adds lua-loader. A builtin Lua module which byte-compiles and caches Lua files
 -- (speeds up load times).
 -- vim.loader.enable()
