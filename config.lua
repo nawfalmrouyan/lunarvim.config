068bbd70e532bfa1b8bc01cfd4fcafcd89fb3f73
Nawfal <nawfalmrouyan@gmail.com>
2024-01-10 15:23:04 +0800

    autocmd to tsserver - auto import missing imports when save. test

--- config.lua
+++ config.lua
@@ -732,6 +732,17 @@ lvim.plugins = {
     ft = { "html", "css", "javascript", "typescript", "javascriptreact", "typescriptreact", "svelte" },
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     opts = {},
+    init = function()
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = vim.api.nvim_create_augroup("TS_add_missing_imports", { clear = true }),
+        desc = "TS_add_missing_imports",
+        pattern = { ".ts", ".tsx", ".js", ".jsx" },
+        callback = function()
+          vim.cmd [[TSToolsAddMissingImports]]
+          vim.cmd "write"
+        end,
+      })
+    end,
   },
   {
     "chrisgrieser/nvim-recorder",
