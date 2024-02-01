0320af88e335b99bdd7eedbd585784d9a4a137e2
Nawfal <nawfalmrouyan@gmail.com>
2024-02-01 12:32:24 +0800

    add kitty-scrollback plugin

--- config.lua
+++ config.lua
@@ -967,6 +967,18 @@ lvim.plugins = {
     event = "BufRead",
     ft = { "rust" },
   },
+  {
+    "mikesmithgh/kitty-scrollback.nvim",
+    enabled = true,
+    lazy = true,
+    cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
+    event = { "User KittyScrollbackLaunch" },
+    -- version = '*', -- latest stable version, may have breaking changes if major version changed
+    -- version = '^4.0.0', -- pin major version, include fixes and features that do not have breaking changes
+    config = function()
+      require("kitty-scrollback").setup()
+    end,
+  },
 }
 
 lvim.autocommands = {
