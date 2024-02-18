4a07457197e7146a616c6d426b2c543e69e2de39
Nawfal <nawfalmrouyan@gmail.com>
2024-02-18 17:00:06 +0800

    neovide configs

--- config.lua
+++ config.lua
@@ -29,6 +29,27 @@ vim.opt.colorcolumn = "100"
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = "number"
 
+if vim.g.neovide then
+  -- Helper function for transparency formatting
+  local alpha = function()
+    return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+  end
+  -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+  vim.g.neovide_transparency = 0.7
+  vim.g.transparency = 0.8
+  vim.g.neovide_background_color = "#1E1E2F" .. alpha()
+
+  -- floating blur
+  vim.g.neovide_floating_blur_amount_x = 2.0
+  vim.g.neovide_floating_blur_amount_y = 2.0
+
+  -- floating shadow
+  vim.g.neovide_floating_shadow = true
+  vim.g.neovide_floating_z_height = 10
+  vim.g.neovide_light_angle_degrees = 45
+  vim.g.neovide_light_radius = 5
+end
+
 -- #22668 adds lua-loader. A builtin Lua module which byte-compiles and caches Lua files
 -- (speeds up load times).
 vim.loader.enable()
