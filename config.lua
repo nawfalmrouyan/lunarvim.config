7554569268887eb90abd6ea5c8314b4ce5fff5ae
Nawfal <nawfalmrouyan@gmail.com>
2024-02-18 16:59:44 +0800

    disable LV transparent window

--- config.lua
+++ config.lua
@@ -7,7 +7,7 @@ lvim.lint_on_save = false
 lvim.colorscheme = "catppuccin"
 lvim.builtin.lualine.options.theme = "catppuccin"
 lvim.shell = "/usr/bin/zsh"
-lvim.transparent_window = true
+lvim.transparent_window = false
 
 -- vim.opt.cmdheight = 0
 -- vim.opt.timeoutlen = 500
