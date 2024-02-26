944b9c203b1ff57628da24f7d058bd62d694ba30
Nawfal <nawfalmrouyan@gmail.com>
2024-02-26 14:35:42 +0800

    add chatgpt plugin

--- config.lua
+++ config.lua
@@ -251,6 +251,19 @@ lvim.plugins = {
     end,
   },
   {
+    "dreamsofcode-io/ChatGPT.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("chatgpt").setup {
+        async_api_key_cmd = "pass show chatgpt",
+      }
+    end,
+  },
+  {
     "dstein64/vim-startuptime",
     cmd = "StartupTime",
     config = function()
