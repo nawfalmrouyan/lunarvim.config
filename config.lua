51f846b6ff4575513e43b859e6bd0e33a28888c4
Nawfal <nawfalmrouyan@gmail.com>
2024-02-27 20:04:31 +0800

    Update nvim-ufo config. Make the config inline.

--- config.lua
+++ config.lua
@@ -329,7 +329,20 @@ lvim.plugins = {
     event = "BufRead",
     dependencies = "kevinhwang91/promise-async",
     config = function()
-      require("user.nvim-ufo").config()
+      vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+      vim.o.foldcolumn = "1"
+      vim.o.foldlevel = 99 -- feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+      vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+
+      require("ufo").setup {
+        provider_selector = function(bufnr, filetype, buftype)
+          return { "treesitter", "indent" }
+        end,
+      }
     end,
   },
   {
