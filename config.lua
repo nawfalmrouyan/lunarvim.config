bc1ff28c9da6c1ed3bed2891921ec8f42a7e9f04
Nawfal <nawfalmrouyan@gmail.com>
2024-02-28 09:10:24 +0800

    Include noice setup inline.

--- config.lua
+++ config.lua
@@ -347,10 +347,35 @@ lvim.plugins = {
   },
   {
     "folke/noice.nvim",
-    config = function()
-      require("user.noice").config()
-    end,
     dependencies = { "MunifTanjim/nui.nvim" },
+    config = function()
+      require("noice").setup {
+        lsp = {
+          progress = { enabled = true },
+          signature = { enabled = true },
+          hover = { enabled = true },
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
+        presets = {
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = true, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = true, -- add a border to hover docs and signature help
+        },
+        views = {
+          mini = {
+            win_options = {
+              winblend = 0,
+            },
+          },
+        },
+      }
+    end,
   },
   { "preservim/vim-markdown", ft = "markdown", dependencies = "godlygeek/tabular" },
   {
