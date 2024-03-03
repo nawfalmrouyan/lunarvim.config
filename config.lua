8a9afa088d0f07a42211871faa81d54e6ae5e6f0
Nawfal <nawfalmrouyan@gmail.com>
2024-03-03 14:21:23 +0800

    add code screenshot plugin

--- config.lua
+++ config.lua
@@ -251,6 +251,23 @@ lvim.plugins = {
     end,
   },
   {
+    "michaelrommel/nvim-silicon",
+    lazy = true,
+    cmd = "Silicon",
+    init = function()
+      vim.keymap.set("v", "<leader>si", "<cmd>Silicon<cr>", { silent = true })
+    end,
+    config = function()
+      require("silicon").setup {
+        font = "PragmataPro Nerd Font Mono=34",
+        theme = "OneHalfDark",
+        window_title = function()
+          return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
+        end,
+      }
+    end,
+  },
+  {
     "dreamsofcode-io/ChatGPT.nvim",
     event = "VeryLazy",
     dependencies = {
